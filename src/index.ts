199069be99b9936da6490bede35c217ade008869
Brandon Trabon <brandon.trabon@gmail.com>
2025-07-15 23:57:23 +1000

    BHC-8 - Forgot to export the radio group components and types.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { Field } from "./components/common/field/field.component.tsx";
 import { Label } from "./components/common/label/label.component.tsx";
 import { HeadlessProvider } from './architecture/components/headless-provider/headless-provider.component.tsx';
 import { ItemScroller } from "./components/data/item-scroller/item-scroller.component.tsx";
+import { RadioGroup } from './components/form/radio-group/radio-group.component.tsx';
+import { RadioItem } from './components/form/radio-group/radio-item.component.tsx';
 import { Toggle } from './components/form/checkboxes/toggle/toggle.component.tsx';
 
 // functions / hooks
@@ -14,6 +16,8 @@ import { useFieldContext } from "architecture/hooks/use-field-context/use-field.
 import type { ComponentAnimationProps } from "./components/component-interfaces.ts";
 import type { CheckboxChangeEvent } from "./components/form/checkboxes/checkboxes.interfaces.ts";
 import type { CheckboxProps } from './components/form/checkboxes/checkbox/checkbox.props.ts';
+import type { RadioChangeEvent, RadioGroupProps } from './components/form/radio-group/radio-group.props.ts';
+import type { RadioItemProps } from './components/form/radio-group/radio-item.props.ts';
 import type { ToggleProps } from './components/form/checkboxes/toggle/toggle.props.ts';
 import type { WithHeadlessChildRenderProp } from "./architecture/headless-interfaces.ts";
 
@@ -25,6 +29,8 @@ export {
   Label,
   HeadlessProvider,
   ItemScroller,
+  RadioGroup,
+  RadioItem,
   Toggle,
   useHeadlessContext,
   useFieldContext
@@ -34,6 +40,9 @@ export type {
   ComponentAnimationProps,
   CheckboxChangeEvent,
   CheckboxProps,
+  RadioChangeEvent,
+  RadioGroupProps,
+  RadioItemProps,
   ToggleProps,
   WithHeadlessChildRenderProp
 };
